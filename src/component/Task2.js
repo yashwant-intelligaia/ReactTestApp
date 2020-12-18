e7a40c740168ea6742441ecdc11cc52ad9daec56
Yashwant Rautela <yashwant.rautela@intelligaia.com>
2020-12-18 20:58:10 +0530

    task2 changes

--- src/component/Task2.js
+++ src/component/Task2.js
@@ -21,21 +21,23 @@ const useStyles = makeStyles({
 
 export default function Task2() {
   const date = new Date();
-  const hour = (date.getHours() >= 12 ? (date.getHours() - 12) : date.getHours());
-  const minute = (date.getMinutes() >= 10 ? (date.getMinutes()) : ("0" + date.getMinutes()))
-  const seconds = (date.getSeconds() >= 10 ? date.getSeconds() : ("0" + date.getSeconds()))
-  const watchTime = (hour > 9 ? hour : ("0" + hour) + ":" + minute + ":" + seconds);
+  const hour = (date.getHours() > 12 ? (date.getHours() - 12) : date.getHours());
+  const minute = (date.getMinutes() >= 10 ? (date.getMinutes()) : ("0" + date.getMinutes()));
+  const seconds = (date.getSeconds() >= 10 ? date.getSeconds() : ("0" + date.getSeconds()));
+  const period = ((date.getHours()>12)?"PM":"AM")
+  const watchTime = ((hour > 9 ? hour : ("0" + hour)) + ":" + minute + ":" + seconds+ " "+period);
   const [time, setTime] = useState(watchTime);
   const classes = useStyles();
   const backgroundUrl = "https://i.pinimg.com/originals/24/48/24/244824821f943a397973d35a9b5f6748.jpg";
 
   const updateTime = () => {
     let now = new Date();
-    let hour = (now.getHours() >= 12 ? (now.getHours() - 12) : now.getHours());
+    let hour = (now.getHours() > 12 ? (now.getHours() - 12) : now.getHours());
     hour = (hour > 9 ? hour : ("0" + hour));
     let minute = (date.getMinutes() >= 10 ? (date.getMinutes()) : ("0" + date.getMinutes()));
     let seconds = (now.getSeconds() >= 10 ? now.getSeconds() : ("0" + now.getSeconds()));
-    setTime(hour + ":" + minute + ":" + seconds);
+    let period = ((date.getHours()>12)?"PM":"AM")
+    setTime(hour + ":" + minute + ":" + seconds+" "+period);
   }
   return (
     <Card className={classes.root}>
